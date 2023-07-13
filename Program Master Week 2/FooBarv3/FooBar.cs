d46c43dfb195581773cb69c495e7d38647df274b
Bimanjaya Aji <anjay4705@gmail.com>
2023-07-13 21:34:23 +0700

    foobarv3

---
+++ Program Master Week 2/FooBarv3/FooBar.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Text;
+namespace foobar;
+
+class FooBarv2
+{
+    public static List<object> theList = new List<object>();
+    public static Dictionary<int, string> FooBarDict = new Dictionary<int, string>(); 
+    public static int theListLen;
+
+    public static List<object> Init(Dictionary<int, string> dict, int end)
+    {
+        List<int> keys = new List<int>(dict.Keys);
+        keys.Sort();
+        keys.Reverse();
+
+        for (int i = 0; i <= Math.Abs(end); i++)
+        {
+            theList.Add(i);
+        }
+
+        theListLen = theList.Count;
+
+        foreach (var key in keys)
+        {
+            for (int i = 0; i < theListLen; i++)
+            {
+                if (i % key == 0 && i != 0)
+                {
+                    theList[i] = dict[key];
+                }
+            }
+        }
+        return theList;
+    }
+
+    public static List<object> Config(List<object> list, int num, string word)
+    {
+        theList = list;
+        theListLen = list.Count;
+
+        if (num < theListLen)
+        {
+            //Console.WriteLine("inside range");
+            for (int i = 0; i < theListLen; i++)
+            {
+                if (i % num == 0 && i != 0)
+                {
+                    theList[i] = word;
+                }
+            }
+            return theList;
+        }
+        else
+        {
+            //Console.WriteLine("outside range");
+            return theList;
+        }
+    }
+
+    public static List<object> Remove(List<object> list, int num)
+    {
+        theList = list;
+        theListLen = list.Count;
+
+        if (num < theListLen)
+        {
+            //Console.WriteLine("inside range");
+            string word = list[num].ToString();
+
+            if (theList[num].ToString() != num.ToString())
+            {
+                //Console.WriteLine("removeable");
+                for (int i = 0; i < theListLen; i++)
+                {
+                    if (i % num == 0 && i != 0)
+                    {
+                        if (list[i].ToString() == word)
+                        {
+                            theList[i] = i;
+                        }
+                    }
+                }
+                return theList;
+            }
+            else
+            {
+                //Console.WriteLine("nothing to remove");
+                return theList;
+            }
+        }
+        else
+        {
+            //Console.WriteLine("outside range");
+            return theList;
+        }
+    }
+
+    public static List<object> SetEndPoint(List<object> list, int end)
+    {
+        theList = list;
+        theListLen = list.Count;
+
+        if (end < 0)
+        {
+            return theList;
+        }
+        else
+        {
+            if (end != theListLen - 1)
+            {
+                if (end < theListLen - 1)
+                {
+                    for (int i = theListLen - 1; i >= end + 1; i--)
+                    {
+                        theList.RemoveAt(i);
+                    }
+                    return theList;
+                }
+                else
+                {
+                    FooBarList();
+
+                    for (int i = theListLen; i <= Math.Abs(end); i++)
+                    {
+                        theList.Add(i);
+                    }
+
+                    List<int> keys = new List<int>(FooBarDict.Keys);
+                    foreach (var key in keys)
+                    {
+                        for (int i = theListLen; i <= end; i++)
+                        {
+                            if (i % key == 0 && i != 0)
+                            {
+                                theList[i] = FooBarDict[key];
+                            }
+                        }
+                    }
+
+                    return theList;
+                }
+            }
+            else
+            {
+                return theList;
+            }
+        }
+    }
+
+    public static void FooBarList()
+    {
+        for (int i = 0; i < theListLen; i++)
+        {
+            string? theWord = theList[i].ToString();
+            if (theWord != i.ToString())
+            {
+                if (FooBarDict.ContainsValue(theWord) == false)
+                {
+                    FooBarDict[i] = theList[i].ToString();
+                }
+            }
+        }
+    }
+}
+
+class FooBarv1
+{
+    // declaring dictionary and endPoint data
+    public Dictionary<int, string> theDict = new Dictionary<int, string>();
+    public int endPoint;
+
+    // declaring numbers sequence data from 0 up to the endPoint
+    public List<object> theList = new List<object>();
+
+    // constructor. taking <int,string> dictionary and endPoint
+    public FooBarv1(Dictionary<int, string> dict, int point)
+    {
+        this.theDict = dict;
+        this.endPoint = point;
+    }
+
+    // main method
+    public void FooBarGo()
+    {
+        Process();
+        Displaying();
+    }
+
+    // display method. for the sake of displaying the result
+    public void Displaying()
+    {
+        StringBuilder sb = new StringBuilder();
+        int iter = 0;
+
+        foreach (object item in this.theList)
+        {
+            iter++;
+            sb.Append(item.ToString());
+            if (iter < this.theList.Count)
+            {
+                sb.Append(", ");
+            }
+        }
+        Console.WriteLine(sb.ToString());
+    }
+
+    public void Process()
+    {
+        // sorting the keys so that we know the biggest and the lowest
+        List<int> keys = new List<int>(theDict.Keys);
+        keys.Sort();
+        keys.Reverse();
+
+        // adding numbers from 0 to endPoint to "theList" variable declared earlier
+        for (int i = 0; i <= endPoint; i++)
+        {
+            this.theList.Add(i);
+        }
+
+        // changing "theList" members according to their modulo result
+        foreach (var key in keys) // the denumerator
+        {
+            for (int i = 0; i < this.theList.Count; i++) // iterating for indexing purpose
+            {
+                // converting object to string. then string to number.
+                // already tried to convert object to nums directly, resulting error
+                // still no idea exactly why
+                string? tempStr = this.theList[i].ToString();
+                bool success = int.TryParse(tempStr, out int theNums);
+
+                if (success)
+                {
+                    if (theNums % key == 0 && theNums != 0)
+                    {
+                        this.theList[i] = theDict[key];
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program Master Week 2/FooBarv3/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Text;
+namespace foobar;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<int, string> myDict = new Dictionary<int, string>();
+        myDict[3] = "foo";
+        myDict[5] = "bar";
+        int endPoint = 100;
+
+        // Initializing new foobar list in "myFooBar" variable
+        List<object> myFooBar = FooBarv2.Init(myDict, endPoint);
+        Displaying(myFooBar);
+        Console.WriteLine("----------");
+        // Adding 15 as "foobar"
+        myFooBar = FooBarv2.Config(myFooBar, 15, "foobar");
+        Displaying(myFooBar);
+        Console.WriteLine("----------");
+        // Changing 15 as "limolas"
+        myFooBar = FooBarv2.Config(myFooBar, 15, "limolas");
+        Displaying(myFooBar);
+        Console.WriteLine("----------");
+        // Reassign 3 back as a number
+        myFooBar = FooBarv2.Remove(myFooBar, 3);
+        Displaying(myFooBar);
+        Console.WriteLine("----------");
+        // Changing the endPoint to 150
+        myFooBar = FooBarv2.SetEndPoint(myFooBar, 150);
+        Displaying(myFooBar);
+        Console.WriteLine("----------");
+        // Changing the endPoint to 50
+        myFooBar = FooBarv2.SetEndPoint(myFooBar, 50);
+        Displaying(myFooBar);
+        Console.WriteLine("----------");
+    }
+
+    public static void Displaying(List<object> obj)
+    {
+        StringBuilder sb = new StringBuilder();
+        int iter = 0;
+
+        foreach (object item in obj)
+        {
+            iter++;
+            sb.Append(item.ToString());
+            if (iter < obj.Count)
+            {
+                sb.Append(", ");
+            }
+        }
+        Console.WriteLine(sb.ToString());
+    }
+}
+
+class Programv1
+{
+    static void Mainv1(string[] args)
+    {
+        Dictionary<int, string> myDict = new Dictionary<int, string>();
+
+        myDict[3] = "telu";
+        myDict[5] = "limo";
+        myDict[15] = "limolas";
+        myDict[7] = "pitu";
+
+        int endPoint = 100;
+
+        FooBarv1 foobar = new FooBarv1(myDict, endPoint);
+        foobar.FooBarGo();
+
+        Console.ReadLine();
+    }
+}
